47fefdf06247ab46ba227c11b8789c448535d285
Gstn <leivagsj@gmail.com>
2020-11-13 00:47:04 -0300

    Game working but error found with key pressed opposite to direction

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ curses.curs_set(0)
 sh, sw = s.getmaxyx()
 w=curses.newwin(sh,sw,0,0)
 w.keypad(1)
-w.timeout(100)
+w.timeout(75)
 
-snk_x= sw/4
-snk_y= sh/2
+snk_x= sw//4
+snk_y= sh//2
 
 snake= [
     [snk_y, snk_x],
@@ -17,7 +17,7 @@ snake= [
     [snk_y, snk_x-2],
 ]
 
-food = [sh/2, sw/2]
+food = [sh//2, sw//2]
 w.addch(food[0], food[1], curses.ACS_PI)
 
 key = curses.KEY_RIGHT
